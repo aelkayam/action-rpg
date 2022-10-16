d8c3f1a985441460f16118be8bc62200e5297f9a
Elkayam Melesse <elka@duck.com>
2022-10-16 12:23:39 +0300

    Added heart sound effect

--- World/Heart.gd
+++ World/Heart.gd
@@ -1,6 +1,10 @@
 extends Area2D
 
+const CollectHeartEffect = preload("res://World/CollectHeartEffect.tscn")
+
 
 func _on_body_entered(_body: Node) -> void:
 	PlayerStats.health += 1
+	var collectHeartEffect = CollectHeartEffect.instance()
+	get_parent().add_child(collectHeartEffect)
 	queue_free()
